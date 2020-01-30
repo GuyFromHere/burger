7345546f3b45b4e1b672b0eec9bc28fcaa858834
Dan Dismuke <dandismuke@gmail.com>
2020-01-30 11:19:47 -0800

    updated connections to use clearDB.

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+/* const connection = mysql.createConnection({
     host: "localhost",
     user: "dbUser",
     password: "How do you like my password?",
     database: "burgers_db",
+    multipleStatements: true
+});
+ */
+
+const connection = mysql.createConnection({
+    host: "us-cdbr-iron-east-04.cleardb.net",
+    user: "b14a96e9bf350e",
+    password: "d2692974",
+    database: "heroku_9e29eacc6b8a972",
     multipleStatements: true
 });
 
